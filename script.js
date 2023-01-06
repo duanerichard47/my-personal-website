d5c55069e1227a09058346e30cda4faafd2cbc37
Duane <duanerichard47@hotmail.com>
2023-01-06 04:40:12 -0500

    updated front end

--- script.js
+++ script.js
@@ -4,4 +4,4 @@ document.querySelector("body").appendChild(h2);
 
 const h4 = document.createElement("h4");
 h4.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2);
+document.querySelector("body").appendChild(h4);
